ab0bd0aa115437448e2c51edb755ba09db9c5c5f
rohit <rohitkedar2000@gmail.com>
2024-01-30 20:54:51 +0530

    added logout functionality

--- src/Components/SuggestedUsers/SuggestedHeader.jsx
+++ src/Components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,25 +1,40 @@
-import { Avatar, Flex, Link, Text } from "@chakra-ui/react"
-import { Link as RouterLink } from "react-router-dom"
+import { Avatar, Button, Flex,  Text } from "@chakra-ui/react"
+import useLogout from '../../Hooks/useLogout';
+import useAuthStore from "../../Store/authStore";
+import { Link } from "react-router-dom";
+
 
 function SuggestedHeader() {
+  const {handleLogout,isLoggingOut,} = useLogout()
+  const authUser = useAuthStore((state) => state.user);
+  if (!authUser) return null;
   return (
+    
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
       
       <Flex alignItems={"center"} gap={2}> 
-        <Avatar name='As a Program' size={"lg"} src="/profilepic.png"/>
+      <Link to={`${authUser.username}`}>
+        <Avatar  size={"lg"} src={authUser.profilePicUrl}/>
+      </Link>
+      <Link to={`${authUser.username}`}>
         <Text fontSize={12} fontWeight={"bold"}>
-          programmer
+           {authUser.username} 
         </Text>
+      </Link>
       </Flex>
-      <Link
-      as={RouterLink}
-      to={'/auth'}
+      <Button
+      size={"xs"}
+      background={"transparent"}
+      _hover={{background: "transparent"}}
       fontSize={14}
       fontWeight={"medium"}
       color={"blue.400"}
-      style={{textDecoration:"none"}}
       cursor={"pointer"}
-  >Log out</Link>
+      onClick={handleLogout}
+      isLoading={isLoggingOut}
+      >
+        Log out
+        </Button>
       </Flex>
 
   )

1b5dead76718018384496251b786217fce850f01
rohit <rohitkedar2000@gmail.com>
2024-01-31 15:17:23 +0530

    added googleAuth for logging in

--- src/Components/AuthForm/GoogleAuth.jsx
+++ src/Components/AuthForm/GoogleAuth.jsx
@@ -1,10 +1,55 @@
 import { Flex, Image, Text } from "@chakra-ui/react"
+import { useSignInWithGoogle } from "react-firebase-hooks/auth"
+import { auth, firestore } from "../../Firebase/firebase"
+import  useShowToast  from "../../Hooks/useShowToast"
+import useAuthStore  from "../../Store/authStore"
+import { doc, setDoc } from "firebase/firestore"
 
-function GoogleAuth() {
+function GoogleAuth({prefix}) {
+
+  const [signInWithGoogle, , , error] =useSignInWithGoogle(auth);
+  const showToast = useShowToast();
+  const loginUser = useAuthStore((state)=> state.login);
+
+
+
+    const handleGoogleAuth = async ()=>{
+      try{
+        
+        const newUser = await signInWithGoogle();
+        if(!newUser && error){
+          showToast("Error",error.message,"error");
+          return ;
+        }
+
+        if(newUser){
+          const userDoc = {
+          uid:newUser.user.uid,
+          email: newUser.user.email,
+          username:newUser.user.email.split("@")[0],
+          fullName:newUser.user.displayName,
+          bio:"",
+          profilePicUrl:newUser.user.photoURL,
+          followers:[],
+          following:[],
+          posts:[],
+          createdAt:Date.now(),
+        };
+        await setDoc(doc(firestore,"users",newUser.user.uid),userDoc);
+        localStorage.setItem("user-info",JSON.stringify(userDoc));
+        loginUser(userDoc);
+    }
+      }catch(error){
+        showToast("Error",error.message,"error");
+
+
+      }
+    }
+
   return (
-    <Flex alignItems={"center"} justifyContent={"center"}  cursor={"pointer"}>
+    <Flex alignItems={"center"} justifyContent={"center"}  cursor={"pointer"} onClick={handleGoogleAuth}>
 <Image src="/google.png" w={5} alt="Google logo" />
-    <Text mx="2" color={"blue.500"}>login with google  </Text>
+    <Text mx="2" color={"blue.500"}>{prefix} with google  </Text>
     </Flex>
   )
 }

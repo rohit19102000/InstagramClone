d1e844d4be9f0110100eee70c552890642c4631a
rohit <rohitkedar2000@gmail.com>
2024-02-12 12:23:38 +0530

    added queries in theme

--- src/main.jsx
+++ src/main.jsx
@@ -9,13 +9,24 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 const styles = {
-	global: (props) => ({
-		body: {
-			bg: mode("gray.100", "#000")(props),
-			color: mode("gray.800", "whiteAlpha.900")(props),
-		},
-	}),
+  global: (props) => ({
+    body: {
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAlpha.900")(props),
+    },
+  }),
+  html: {
+    "@media (prefers-color-scheme: light)": {
+      background: "gray.100",
+      color: "gray.800",
+    },
+    "@media (prefers-color-scheme: dark)": {
+      background: "#000",
+      color: "whiteAlpha.900",
+    },
+  },
 };
+
 
 const config = {
 	initialColorMode: "dark",
